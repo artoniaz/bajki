0cbf823b4c0f33dde08583564a96c8423d8b5f4e
Artur <artoni.az@gmail.com>
2024-02-08 20:50:16 +0100

    Hero update

--- bajki-front/src/pages/Home.js
+++ bajki-front/src/pages/Home.js
@@ -1,32 +1,49 @@
-import { Box, Button, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { navItems } from "../Router";
 
 const Home = () => {
   return (
-    <Grid container spacing={0} alignItems="center" justifyContent="center">
+    <Grid container spacing={0} alignItems="end">
       <Box
         component="img"
         sx={{
-          height: "100vh",
           width: "100%",
-          opacity: 0.85,
+          maxWidth: "3400px",
+          height: "100vh",
+          objectFit: "cover",
+          margin: "auto",
         }}
         alt="Children reading, having fun."
         src={require("../images/coverImg.jpg")}
-      ></Box>
+      />
       <Box
-        component="div"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "flex-start",
+          top: 0,
+          left: 0,
+          height: "100vh",
+          width: "100%",
+          opacity: 0.2,
           position: "absolute",
+          backgroundImage: "linear-gradient(to top, #000000 , #ffffff 70%)",
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          m: 10,
+          color: "white",
         }}
       >
-        <h2>Bajki dla dzieci na dowolny temat</h2>
-        <p>Wybierz temat z dzieckiem</p>
+        <Typography variant="h1">Bajki dla dzieci na dowolny temat</Typography>
+        <Typography variant="h4" sx={{ py: 2 }}>
+          Wybierz temat z dzieckiem
+        </Typography>
         <Button
           component={NavLink}
           to={navItems.createTale.path}
