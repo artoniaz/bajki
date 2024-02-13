a028af2339e6604046f281f02a5a2ebf8368864b
Artur <artoni.az@gmail.com>
2024-02-13 19:59:47 +0100

    Created backend route authorization

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,17 @@
 const express = require("express");
-
-// controller methods
-const { loginUser, registerUser, getUserProfile } = require("../controllers/userController");
+const {
+  loginUser,
+  registerUser,
+  getUserProfile,
+} = require("../controllers/userController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
+
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-//TODO: protect the route
-// router.route('/profile').get(protect, userController.getUserProfile)
+router.use(requireAuth);
 router.get("/profile/:_id", getUserProfile);
 
 module.exports = router;
