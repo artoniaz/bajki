1ee0031362c59f91fd0c91dd54a7199b1c5b1998
Artur <artoni.az@gmail.com>
2024-02-17 14:32:52 +0100

    Handle redux reset on user logout

--- bajki-front/src/features/store.ts
+++ bajki-front/src/features/store.ts
@@ -1,14 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createTaleReducer from "./tales/createTale/createTaleSlice";
 import authReducer from "./auth/authSlice";
 import getTalesReducer from "./tales/getTales/getTalesSlice";
 
+const combinedReducers = combineReducers({
+  auth: authReducer,
+  createTale: createTaleReducer,
+  getTales: getTalesReducer,
+});
+
+export const rootReducer = (state: any, action: any) => {
+  if (action.type === "auth/logoutUser") {
+    return combinedReducers(undefined, action);
+  }
+  return combinedReducers(state, action);
+};
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    createTale: createTaleReducer,
-    getTales: getTalesReducer,
-  },
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
