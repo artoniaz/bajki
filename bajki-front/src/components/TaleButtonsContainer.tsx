94e4a718ef66d4a7d9160ff575853075dd6fde32
Artur <artoni.az@gmail.com>
2024-03-11 21:19:30 +0100

    Tale buttons container mobile

--- bajki-front/src/components/TaleButtonsContainer.tsx
+++ bajki-front/src/components/TaleButtonsContainer.tsx
@@ -1,22 +1,22 @@
 import { Box, Button } from "@mui/material";
+import { isMobile } from "react-device-detect";
 
 const TaleButtonsContainer = () => {
   return (
     <Box
-      position="sticky"
+      position={isMobile ? "static" : "sticky"}
       bottom="0"
       bgcolor="#fff"
       width="100%"
-      mr={10}
       display="flex"
       justifyContent="space-between"
-      p={3}
+      p={isMobile ? 0 : 3}
       borderRadius="48px 48px 0 0"
-      border="1px solid #000"
+      border={isMobile ? "none" : "1px solid #000"}
       borderBottom="none"
     >
       {/* TODO: onclick */}
-      <Button sx={{ flexGrow: "1", mr: 8 }} variant="outlined">
+      <Button sx={{ flexGrow: "1", mr: isMobile ? 2 : 8 }} variant="outlined">
         Stwórz nową
       </Button>
       {/* TODO: onclick */}
