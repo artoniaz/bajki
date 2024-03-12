3e90dd2827cbb20cdad37b2742a495f3a61d369b
Artur <artoni.az@gmail.com>
2024-03-12 11:28:23 +0100

    Fixed tales feed single item ui bug

--- bajki-front/src/features/tales/getTales/TalesFeed.tsx
+++ bajki-front/src/features/tales/getTales/TalesFeed.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../../../hooks/reduxHooks";
 import taleActions from "../talesActions";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import TalesContainer from "../../../components/TalesContainer";
 
 const TalesFeed = () => {
@@ -12,16 +12,19 @@ const TalesFeed = () => {
   }, []);
 
   return (
-    <>
-      <Typography mb={3} color="secondary.main" variant="h2"
-      sx={{
-        mt: {xs: 8, md: 2}
-      }}
+    <Box>
+      <Typography
+        mb={3}
+        color="secondary.main"
+        variant="h2"
+        sx={{
+          mt: { xs: 8, md: 2 },
+        }}
       >
         Moje bajki
       </Typography>
       <TalesContainer />
-    </>
+    </Box>
   );
 };
 
