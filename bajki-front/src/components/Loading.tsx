f0d64048c1d087fc81b0ea7c2552da8b45fcdf94
Artur <artoni.az@gmail.com>
2024-03-19 11:49:01 +0100

    Handle create tale loading animation

--- bajki-front/src/components/Loading.tsx
+++ bajki-front/src/components/Loading.tsx
@@ -1,14 +1,15 @@
 import { useTheme } from "@mui/material";
 import ReactLoading from "react-loading";
 
-const Loading = (width : {width?: number}) => {
+const Loading = ({size} : {size?: number}) => {
   const theme = useTheme();
 
   return (
     <ReactLoading
       type="bubbles"
       color={theme.palette.primary.main}
-      width={width ?? 100}
+      width={size ?? 100}
+      height={size}
     />
   );
 };
